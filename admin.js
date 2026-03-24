cfe174485596c0d26825843a0e311526fad38618
Aronsv <49215005+Aronsv@users.noreply.github.com>
2026-03-24 13:51:56 -0500

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+<meta charset="UTF-8">
+<title>Panel Admin</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="card">
+
+<h1 class="logo">☕ Panel Admin</h1>
+
+<div id="listaUsuarios">
+
+Cargando empleados...
+
+</div>
+
+</div>
+
+<script type="module" src="admin.js"></script>
+
+</body>
+</html>

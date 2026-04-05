eac6cbce57121ce61d825c45edfda69f80361045
Aronsv <49215005+Aronsv@users.noreply.github.com>
2026-04-05 15:20:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -950,7 +950,11 @@ function iniciarAdmin() {
       if (sec === 'tareas-admin') iniciarTareasAdmin();
       if (sec === 'finanzas-admin') iniciarFinanzasAdmin();
       if (sec === 'insumos-admin') iniciarInsumosAdmin();
-      if (sec === 'personal') cargarPersonal();
+      if (sec === 'personal') {
+        const secEl = document.getElementById('admin-sec-personal');
+        secEl.style.display = 'flex';
+        cargarPersonal();
+      }
     });
   });
 
